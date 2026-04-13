dcd4a6cd40534ddf1079eb9c10eb981e1c7908b2
stormvejr <>
2026-04-13 14:17:33 +0000

    Challenge: Inventory Sorting Capstone

--- variables_and_types/challenge_inventory_sorting_capstone/main.py
+++ variables_and_types/challenge_inventory_sorting_capstone/main.py
@@ -13,5 +13,7 @@ bubblegum_price = "$1.50"
 chocolate_price = "$2.00"
 pasta_price = "$5.40"
 
-
+print("We have " + candy1 + " for " + bubblegum_price + " in the " + category1)
+print("We have " + candy2+ " for " + chocolate_price + " in the " + category1)
+print("We have " + dry_goods + " for " + pasta_price + " in the " + category2)
 
